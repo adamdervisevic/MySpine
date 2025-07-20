c26e72f537b3b0be5c908bfbfd509295413af078
Adam <adamdervisevic@yahoo.com>
2025-07-21 01:03:45 +0700

    Prvi commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Navbar2 } from './components/Navbar2';
+import {Header7} from './components/Header7';
+import {Layout22} from './components/Layout22';
+import {Gallery18} from './components/Gallery18';
+import {Gallery20} from './components/Gallery20';
+import {Testimonial19} from './components/Testimonial19';
+import {Footer4} from './components/Footer4';
+
+export default function App() {
+  return (
+    <div>
+          <Navbar2 />
+          <Header7 />
+          <Layout22 />
+          <Gallery18 />
+          <Gallery20 />
+          <Testimonial19 />
+          <Footer4 /> 
+    </div>
+  );
+}
+
+
